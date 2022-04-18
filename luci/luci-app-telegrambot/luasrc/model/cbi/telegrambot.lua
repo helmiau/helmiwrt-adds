36c5d4c050a8abe9c056ddf6beb776111cb38e9b
Helmi Amirudin <helmilaw@gmail.com>
2022-04-18 19:56:03 +0700

    Update telegrambot.lua
    update info and instructions

--- luci/luci-app-telegrambot/luasrc/model/cbi/telegrambot.lua
+++ luci/luci-app-telegrambot/luasrc/model/cbi/telegrambot.lua
@@ -6,7 +6,7 @@ require("luci.sys")
 local apply = luci.http.formvalue ("cbi.apply")
 local m, s
 
-s = Map("telegrambot", translate("TelegramBot"), translate("Telegram bot for router with firmware Lede/Openwrt."))
+s = Map("telegrambot", translate("TelegramBot"), translate("Telegram bot for router with firmware Lede/Openwrt. <a href=\"https://github.com/helmiau/helmiwrt-adds/blob/main/packages/net/telegrambot/README.md\">Read more instructions here</a>."))
 m = s:section(TypedSection)
 m.anonymous = true
 m.addremove = false
@@ -14,8 +14,8 @@ enable = m:option(Flag, "enabled", translate("Enable"), translate ("Enable Bot")
 enable.value = 1
 enable.value = 0
 enable.default = 0
-token = m:option(Value, "bot_token", translate("Bot Token"), translate("Token ID your Telegram Bot"))
-chatid = m:option(Value, "chat_id", translate("Bot ID"), translate("Chat ID your Telegram Bot"))
+token = m:option(Value, "bot_token", translate("Bot Token"), translate("Fill with your Telegram Bot Token. Get your Bot Token by <a href=\"https://t.me/get_id_bot\">@get_id_bot</a> telegram bot."))
+chatid = m:option(Value, "chat_id", translate("Bot ID"), translate("Chat ID your Telegram Bot. Get your Bot Token by <a href=\"https://t.me/BotFather\">@BotFather</a> telegram bot."))
 timeout = m:option(Value, "timeout", translate("Time Out"), translate("Time Out respone Bot in s."))
 ptime = m:option(Value, "polling_time", translate("Polling Time"), translate ("Polling Time in s."))
 plugins = m:option(Value, "plugins", translate("Plugins"), translate("Path to plugins directory."))
